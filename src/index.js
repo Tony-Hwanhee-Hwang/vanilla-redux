b8e26a2f524439ffc7d715f3ee4a001328106655
Tony.Hwnag <tony.hwang.developer@gmail.com>
2020-04-06 19:43:20 -0400

    make simple vanilla script counter

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+let count = 0;
+
+number.innerText = count;
+const updateText = () => {
+	number.innerText = count;
+};
+
+const handleAdd = (event) => {
+	count++;
+	updateText();
+};
+
+const handleMinus = (event) => {
+	count--;
+	updateText();
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
