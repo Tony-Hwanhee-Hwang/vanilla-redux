28fec458f08a83860b74ea904a68bbbeff8d36c2
Tony.Hwnag <tony.hwang.developer@gmail.com>
2020-04-06 23:37:57 -0400

    change vinilla js using redux

--- src/index.js
+++ src/index.js
@@ -4,12 +4,61 @@ const input = document.querySelector("input");
 const btn = document.querySelector("button");
 const ul = document.querySelector("ul");
 
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const addToDo = (text) => {
+	return { type: ADD_TODO, text: text };
+};
+const deleteTodo = (id) => {
+	return { type: DELETE_TODO, id: id };
+};
+const handleTodo = (state = [], action) => {
+	switch (action.type) {
+		case ADD_TODO:
+			return [...state, { text: action.text, id: Date.now() }];
+		case DELETE_TODO:
+			console.log(state.filter((item) => item.id !== action.id));
+			return state.filter((item) => item.id !== action.id);
+		default:
+			return state;
+	}
+};
+
+const todolist = createStore(handleTodo);
+
+const dispatchAddToDo = (text) => {
+	todolist.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (e) => {
+	console.log(e.target.parentNode.id);
+	const id = parseInt(e.target.parentNode.id);
+	todolist.dispatch(deleteTodo(id));
+};
+
+const paintToDos = () => {
+	const todos = todolist.getState();
+	ul.innerText = "";
+	todos.forEach((item) => {
+		let li = document.createElement("li");
+		li.id = item.id;
+		li.innerText = item.text;
+		let deleteBtn = document.createElement("button");
+		deleteBtn.innerText = "DEL";
+		deleteBtn.addEventListener("click", dispatchDeleteToDo);
+		li.appendChild(deleteBtn);
+		ul.appendChild(li);
+	});
+};
+
+todolist.subscribe(paintToDos);
+
 const onSubmit = (e) => {
 	e.preventDefault();
-	let li = document.createElement("li");
-	li.innerText = input.value;
+	const todo = input.value;
 	input.value = "";
-	ul.appendChild(li);
+	dispatchAddToDo(todo);
 };
 
 btn.addEventListener("click", onSubmit);
